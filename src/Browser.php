9e644cc692f08babe8d1e4e20e44f66524443564
Michael Price <mike@edwardrobertson.co.uk>
2019-08-22 14:59:44 +0100

    add constructor methods to Browser class

--- src/Browser.php
+++ src/Browser.php
@@ -5,6 +5,7 @@ namespace EdwardRobertson\SagePayDirect;
 class Browser
 {
     public $accepts;
+    public $challengeWindowSize;
     public $colorDepth;
     public $javaEnabled = 0;
     public $javascriptEnabled = 0;
@@ -13,4 +14,30 @@ class Browser
     public $screenWidth;
     public $tz;
     public $userAgent;
+
+    public function __construct()
+    {
+        $this->getPropertiesFromForm();
+        $this->getPropertiesFromRequestHeaders();
+    }
+
+    private function getPropertiesFromForm()
+    {
+        $this->challengeWindowSize = $_POST['sp4_ChallengeWindowSize'];
+        $this->colorDepth = $_POST['sp4_ColourDepth'];
+        $this->javaEnabled = $_POST['sp4_JavaEnabled'];
+        $this->javascriptEnabled = $_POST['sp4_JavascriptEnabled'];
+        $this->language = $_POST['sp4_Language'];
+        $this->screenHeight = $_POST['sp4_ScreenHeight'];
+        $this->screenWidth = $_POST['sp4_ScreenWidth'];
+        $this->tz = $_POST['sp4_TZ'];
+    }
+
+    private function getPropertiesFromRequestHeaders()
+    {
+        $headers = getallheaders();
+
+        $this->accepts = $headers['Accept'] ?? '';
+        $this->userAgent = $headers['User-Agent'] ?? '';
+    }
 }
